9f445311bbd5d6a2ca8ed8fd999cc0dd1a6cb030
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-13 10:44:03 +0530

    calculator app

---
+++ calculator/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import React from "react";
+import Calculator from "./components/Calculator";
+
+function App() {
+  return <Calculator />;
+}
+
+export default App;
---
+++ calculator/src/components/CalculatorKey.js
@@ -0,0 +1,4 @@
+import React from "react";
+import CalculatorKey from "./CalculatorKey";
+import "./Calculator.css";
+
